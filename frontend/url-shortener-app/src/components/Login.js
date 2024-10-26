2773005a6ae88b5daf6ead32c8417dfc19c0fa6b
ramesh02001 <137371718+ramesh02001@users.noreply.github.com>
2024-10-26 12:58:45 +0530

    Update Login.js

--- frontend/url-shortener-app/src/components/Login.js
+++ frontend/url-shortener-app/src/components/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
         setError('');
 
         try {
-            const response = await fetch('https://bulkemail2001.onrender.com/api/auth/login', {
+            const response = await fetch('https://shortenurl-1-lvaa.onrender.com/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
